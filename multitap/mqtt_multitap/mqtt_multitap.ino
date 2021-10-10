4a5ad488b608acd414d749f0840b275ad612b208
HanUngJae <han494127@gmail.com>
2021-10-10 21:20:43 +0900

    10/10 update

--- multitap/mqtt_multitap/mqtt_multitap.ino
+++ multitap/mqtt_multitap/mqtt_multitap.ino
@@ -1,9 +1,17 @@
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
+#include <ArduinoJson.h>
 
-const char* ssid = "";
-const char* password = "";
-const char* mqtt_server = "broker.mqtt-dashboard.com";
+//int trigPin = D4;   // 초음파센서 출력핀
+//int echoPin = D5;   // 초음파센서 입력핀
+int relay = D3;     // 릴레이모듈 출력핀
+int red_led = D4;   // red_led 출력 --> Relay OFF
+int green_led = D5; // green_led 출력 --> Relay ON Timer ON
+int blue_led = D6;  // blue_led 출력 --> Relay ON Timer OFF
+
+const char* ssid = "HUJ";
+const char* password = "159753asd";
+const char* mqtt_server = "192.168.0.80";
 
 WiFiClient espClient;
 PubSubClient client(espClient);
@@ -11,21 +19,27 @@ unsigned long lastMsg = 0;
 #define MSG_BUFFER_SIZE  (50)
 char msg[MSG_BUFFER_SIZE];
 int value = 0;
+float distance = 0;
+int multitabstate = 0;
+int timestate = 0;
+char get_data[3];
 
-int trigPin = D4; // 초음파센서 출력핀
-int echoPin = D5; // 초음파센서 입력핀
-int relay = D3; // 릴레이모듈 출력핀
+int temp = 0;
+
+StaticJsonBuffer<200> jsonBuffer;
+JsonObject& root = jsonBuffer.createObject();
 
 void setup_wifi() {
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
 
   delay(10);
   // We start by connecting to a WiFi network
   Serial.println();
   Serial.print("Connecting to ");
   Serial.println(ssid);
-
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
+  temp = temp + 1;
 
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
@@ -36,51 +50,77 @@ void setup_wifi() {
 
   Serial.println("");
   Serial.println("WiFi connected");
-  Serial.println("IP address: ");
+  Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
 }
 
 void callback(char* topic, byte* payload, unsigned int length) {
   Serial.print("Message arrived [");
   Serial.print(topic);
-  Serial.print("] ");
+  Serial.print("]: ");
   for (int i = 0; i < length; i++) {
     Serial.print((char)payload[i]);
   }
   Serial.println();
-
   String topic_a = topic;
-  
-  if(topic_a == "multitap"){
-    if ((char)payload[0] == '1') { //on 입력했을 경우
-        digitalWrite(relay, HIGH);
-      } else if((char)payload[0] == '0') { //off 입력했을 경우
-        digitalWrite(relay, LOW);
-      } else if((char)payload[0] == '2') { //auto 입력했을 경우
-        while(1){
-            digitalWrite(trigPin, HIGH);
-            delay(10);
-            digitalWrite(trigPin, LOW);
-            float duration = pulseIn(echoPin, HIGH); //echoPin이 HIGH를 유지한 시간(초음파가 보냈다가 다시 들어온 시간)
-            float distance = ((float)(340 * duration) / 10000) / 2; // 물체와의 거리
-
-            Serial.print(distance);
-            Serial.println("cm");          
-
-            if(distance <= 30) { //30cm내에 물체가 있을경우
-              digitalWrite(relay, HIGH); // 릴레이 on
-            }
-            else{
-              digitalWrite(relay, LOW); // 없을경우 릴레이 off
-            }
-            delay(200);
-
-            if((char)payload[0] == '0'){ //off를 입력했을경우
-              break; //거리로 멀티탭 조정을 멈춤
-            }
-          }  
-      } 
+  int index = 0;
+  for(int i = 0; i < length; i++) {
+    if((char)payload[i] != '?') {
+      get_data[index] = (char)payload[i];
+      index = index + 1;
     }
+  }
+  if(topic_a.equals("multitab")) {
+    // Relay state change OFF
+    if(get_data[0] == '0') {
+      digitalWrite(relay, LOW); //Relay OFF
+      Serial.println("Relay OFF");
+      multitabstate = 0;
+      if(get_data[1] == '0') {
+        digitalWrite(red_led, LOW);
+        digitalWrite(green_led, LOW);
+        digitalWrite(blue_led, LOW);
+        timestate = 0;
+      }
+      else if(get_data[1] == '1') {
+        digitalWrite(blue_led, HIGH);
+        digitalWrite(red_led, LOW);
+        digitalWrite(green_led, LOW);
+        Serial.println("Timer set ON");
+        timestate = 1;
+      }
+    }
+    // Relay state change ON
+    else if (get_data[0] == '1') {
+      digitalWrite(relay, HIGH); //Relay ON
+      Serial.println("Relay ON");
+      multitabstate = 1;
+      Serial.println(get_data[1]);
+      if (get_data[1] == '0') {
+        digitalWrite(red_led, HIGH);
+        digitalWrite(blue_led, LOW);
+        digitalWrite(green_led, LOW);
+        Serial.println("Timer set OFF");
+        timestate = 0;
+      }
+      else if(get_data[1] == '1') {
+        if (get_data[2] == '0') {
+          // 마젠타(분홍색 계열)
+          analogWrite(red_led, 255);
+          analogWrite(green_led, 0);
+          analogWrite(blue_led, 255);
+          Serial.println("Timer ready");
+        }
+        else if (get_data[2] == '1') {
+          digitalWrite(green_led, HIGH);
+          digitalWrite(red_led, LOW);
+          digitalWrite(blue_led, LOW);
+          Serial.println("Timer ~ing");
+        }
+        timestate = 1;
+      }
+    }
+  }
 }
 
 void reconnect() {
@@ -92,11 +132,14 @@ void reconnect() {
     clientId += String(random(0xffff), HEX);
     // Attempt to connect
     if (client.connect(clientId.c_str())) {
+      Serial.println(client.connect(clientId.c_str()));
       Serial.println("connected");
       // Once connected, publish an announcement...
-      client.publish("outTopic", "hello world");
+      //client.publish("outTopic", "hello world");
       // ... and resubscribe
       client.subscribe("multitap");
+      //Serial.print("temp: ");
+      //Serial.println(temp);
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
@@ -109,12 +152,22 @@ void reconnect() {
 
 void setup() {
   pinMode(relay, OUTPUT);
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);    
+  //pinMode(trigPin, OUTPUT);   // 사용 X
+  //pinMode(echoPin, INPUT);    // 사용 X
+  pinMode(red_led, OUTPUT);   // red_led 출력 --> Relay OFF
+  pinMode(green_led, OUTPUT); // green_led 출력 --> Relay ON Timer ON
+  pinMode(blue_led, OUTPUT);  // blue_led 출력 --> Relay ON Timer OFF
   Serial.begin(115200);
+  //digitalWrite(relay, HIGH);
+  Serial.println("이곳 들어오기는 하니???");
+  temp = temp + 1;
   setup_wifi();
   client.setServer(mqtt_server, 1883);
   client.setCallback(callback);
+  digitalWrite(relay, LOW);
+  digitalWrite(red_led, HIGH);
+  delay(1000);
+  digitalWrite(red_led, LOW);
 }
 
 void loop() {
@@ -125,12 +178,16 @@ void loop() {
   client.loop();
 
   unsigned long now = millis();
-  if (now - lastMsg > 2000) {
+  if (now - lastMsg > 5000) {
     lastMsg = now;
     ++value;
-    snprintf (msg, MSG_BUFFER_SIZE, "hello world #%ld", value);
+    root["multitabState"] = multitabstate;
+    root["multitabTime"] = timestate;
+    root.printTo(msg);
+    //snprintf (msg, MSG_BUFFER_SIZE, "hello world #%ld", value);
     Serial.print("Publish message: ");
     Serial.println(msg);
-    client.publish("multitap", msg);
+    client.publish("/shc/multi", msg);
+    client.subscribe("multitab");
   }
 }
